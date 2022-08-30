96a5518ab1c1ec73aa8fe36e1071c7ccaa355b66
nastia12148 <nastia12149@gmail.com>
2022-08-30 14:11:30 +0300

    reading and alerting

--- src/alert_system.py
+++ src/alert_system.py
@@ -1,45 +1,100 @@
 import pandas as pd
+from datetime import datetime
+import pyspark
+#from pyarrow import StructType
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructField, IntegerType, StringType, BooleanType, DoubleType, StructType
 
 
+def log_reader(filepath):
+    df = pd.read_csv(filepath , names=['error_code',
+                                                               'error_message',
+                                                               'severity',
+                                                               'log_location',
+                                                               'mode',
+                                                               'model',
+                                                               'graphics',
+                                                               'session_id',
+                                                               'sdkv',
+                                                               'test_mode',
+                                                               'flow_id',
+                                                               'flow_type',
+                                                               'sdk_date',
+                                                               'publisher_id',
+                                                               'game_id',
+                                                               'bundle_id',
+                                                               'appv',
+                                                               'language',
+                                                               'os',
+                                                               'adv_id',
+                                                               'gdpr',
+                                                               'ccpa',
+                                                               'country_code',
+                                                               'date'],
+                     dtype={'test_mode': str, 'gdpr': str, 'ccpa': str},
+                     skiprows=1)
 
-def first_11_errors(df:pd.DataFrame, bundle_id):
-    if bundle_id:
-        df = df[df["bundle_id"] == bundle_id]
 
+    df = df[['severity', 'bundle_id', 'date']]
+
+    print(df.head())
+
+    return df
+
+
+def alert(df: pd.DataFrame):
     df = df[df["severity"] == "Error"]
-    print(df["error_message"].head(11))
+    row_number = 0
+    beg_time = df["date"].head(1)[0]
+    alert_counter = 0
+
+    while row_number < len(df) - 10:
+        error_list = df[(beg_time - df["date"]) <= 60].head(10)
+        if len(error_list) >= 10:
+            # print("Alert!")
+            # if go_next():
+            row_number += 10
+            alert_counter += 1
+            beg_time = df.iloc[row_number, 23]
+        else:
+            row_number += 1
+            beg_time = df.iloc[row_number, 23]
+
+    return alert_counter
 
 
-df = pd.read_csv('~/PycharmProjects/alert_project/data/data.csv', names=['error_code',
-                                                                         'error_message',
-                                                                         'severity',
-                                                                         'log_location',
-                                                                         'mode',
-                                                                         'model',
-                                                                         'graphics',
-                                                                         'session_id',
-                                                                         'sdkv',
-                                                                         'test_mode',
-                                                                         'flow_id',
-                                                                         'flow_type',
-                                                                         'sdk_date',
-                                                                         'publisher_id',
-                                                                         'game_id',
-                                                                         'bundle_id',
-                                                                         'appv',
-                                                                         'language',
-                                                                         'os',
-                                                                         'adv_id',
-                                                                         'gdpr',
-                                                                         'ccpa',
-                                                                         'country_code',
-                                                                         'date']
-                 ,dtype= {'test_mode':str,'gdpr': str,'ccpa': str},skiprows=1)
+def alert_by_id(df: pd.DataFrame, bundle_id: str):
+    df = df[df["bundle_id"] == bundle_id]
+    df = df[df["severity"] == "Error"]
 
-#print(df["bundle_id"].unique())
-#print(df["error_message"].unique())
-#print(df["severity"].value_counts())
+    row_number = 0
+    beg_time = df["date"].head(1)[0]
 
-first_11_errors(df, False)
+    while True:
+        error_list = df[(beg_time - df["date"]) <= 3600].head(10)
+        if len(error_list) >= 10:
+            print("Alert!")
+        if go_next():
+            row_number += 1
+            beg_time = df.iloc[row_number, 23]
+        else:
+            break
 
-first_11_errors(df, "com.thg.battleops.shooting.game")
+    return error_list
+
+
+def go_next():
+    yes_or_no = input("Do you want to continue?[yes/no]: ")
+    if yes_or_no == "yes":
+        return True
+
+    return False
+
+
+
+df = log_reader('/home/nastia12148/PycharmProjects/data/data.csv')
+
+
+
+
+print(alert(df))
