0126fd1ce6d2189ae874bc5a5b0b4bcfd8fccac1
nastia12148 <nastia12149@gmail.com>
2022-09-02 13:19:51 +0300

    refactored alert_system.py, updated Dockerfile

--- src/alert_system.py
+++ src/alert_system.py
@@ -1,78 +1,74 @@
 import pandas as pd
+import logging
 
 
 def log_reader(filepath):
-    df = pd.read_csv(filepath , names=['error_code',
-                                                               'error_message',
-                                                               'severity',
-                                                               'log_location',
-                                                               'mode',
-                                                               'model',
-                                                               'graphics',
-                                                               'session_id',
-                                                               'sdkv',
-                                                               'test_mode',
-                                                               'flow_id',
-                                                               'flow_type',
-                                                               'sdk_date',
-                                                               'publisher_id',
-                                                               'game_id',
-                                                               'bundle_id',
-                                                               'appv',
-                                                               'language',
-                                                               'os',
-                                                               'adv_id',
-                                                               'gdpr',
-                                                               'ccpa',
-                                                               'country_code',
-                                                               'date'],
-                     dtype={'test_mode': str, 'gdpr': str, 'ccpa': str},
-                     skiprows=1)
+    log_table = pd.read_csv(
+        filepath,
+        names=[
+            "error_code",
+            "error_message",
+            "severity",
+            "log_location",
+            "mode",
+            "model",
+            "graphics",
+            "session_id",
+            "sdkv",
+            "test_mode",
+            "flow_id",
+            "flow_type",
+            "sdk_date",
+            "publisher_id",
+            "game_id",
+            "bundle_id",
+            "appv",
+            "language",
+            "os",
+            "adv_id",
+            "gdpr",
+            "ccpa",
+            "country_code",
+            "date",
+        ],
+        dtype={"test_mode": str, "gdpr": str, "ccpa": str},
+        skiprows=1,
+    )
+
+    log_table = log_table[["severity", "bundle_id", "date"]]
+
+    print(log_table.head())
+
+    return log_table
 
 
-    df = df[['severity', 'bundle_id', 'date']]
-
-    print(df.head())
-
-    return df
-
-
-def alert(df: pd.DataFrame, time:int = 60):
-    df = df[df["severity"] == "Error"]
+def alert(log_table: pd.DataFrame, time: int = 60):
+    log_table = log_table[log_table["severity"] == "Error"]
     row_number = 0
-    beg_time = df["date"].head(1)[0]
+    beg_time = log_table["date"].head(1)[0]
     alert_counter = 0
 
-    while row_number < len(df) - 10:
-        error_list = df[(beg_time - df["date"]) <= time].head(10)
+    while row_number < len(log_table) - 10:
+        error_list = log_table[(beg_time - log_table["date"]) <= time].head(10)
         if len(error_list) >= 10:
             print("Alert!")
             row_number += 1
             alert_counter += 1
-            beg_time = df.iloc[row_number, 2]
+            beg_time = log_table.iloc[row_number, 2]
         else:
             row_number += 1
-            beg_time = df.iloc[row_number, 2]
+            beg_time = log_table.iloc[row_number, 2]
 
     return alert_counter
 
 
-def alert_by_id(df: pd.DataFrame, bundle_id: str):
-    df = df[df["bundle_id"] == bundle_id]
+def alert_by_id(log_table: pd.DataFrame, bundle_id: str):
+    log_table = log_table[log_table["bundle_id"] == bundle_id]
 
-    return alert(df, 3600)
-
-
-'''def go_next():
-    yes_or_no = input("Do you want to continue?[yes/no]: ")
-    if yes_or_no == "yes":
-        return True
-
-    return False'''
+    return alert(log_table, 3600)
 
 
+df = log_reader("data/data.csv")
 
-df = log_reader('data.csv')
-
-#print(alert(df))
-print(alert_by_id(df, 'com.thg.battleops.shooting.game'))
+# print(alert(df))
+print(alert_by_id(df, "com.thg.battleops.shooting.game"))
