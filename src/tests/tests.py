45576b00726b182be058a4b4714f4f2ec692d466
nastia12148 <nastia12149@gmail.com>
2022-09-08 12:50:07 +0300

    added test_alert_by_id

--- src/tests/tests.py
+++ src/tests/tests.py
@@ -1,6 +1,6 @@
-from hypothesis.extra.pandas import data_frames, column, range_indexes
+from hypothesis.extra.pandas import data_frames, column, range_indexes, series
 from hypothesis import given, settings, strategies as st
-from src.alert_system import alert
+from src.alert_system import alert, alert_by_id
 
 
 def hyp_create_dataframe():
@@ -34,9 +34,32 @@ def hyp_create_dataframe():
     return data
 
 
+def hyp_generate_bundle_id():
+    bundle_id = st.sampled_from(
+        [
+            "com.thg.battleops.shooting.game",
+            "com.pregnantcatemma.virtualpet",
+            "com.mytalkingcatemma.ballerinagames",
+            "com.sayollo.cobra_Android_RealBundleId",
+            "com.sayollo.cobra2_Android_RealBundleId",
+            "com.sayollo.JetpackCatGame",
+        ]
+    )
+    return bundle_id
+
+
 @given(df=hyp_create_dataframe())
-@settings(max_examples=50)
-def test_hypothesis(df):
+@settings(max_examples=20)
+def test_alert(df):
     print(df)
     print(alert(df))
+    assert 1
+
+
+@given(df=hyp_create_dataframe(), bundle_id=hyp_generate_bundle_id())
+@settings(max_examples=20)
+def test_alert_by_id(df, bundle_id):
+    print(df)
+    print(bundle_id)
+    print(alert_by_id(df, bundle_id))
     assert 1
